301b37cecace4dac650e0a30094a0d59282fe90f
Carlos C <carlos.cirello.nl@gmail.com>
2015-11-13 18:03:21 +0100

    Treating non indentable comments

--- src/Core/LeftAlignComment.php
+++ src/Core/LeftAlignComment.php
@@ -39,7 +39,6 @@ final class LeftAlignComment extends FormatterPass {
 				continue;
 			}
 			switch ($id) {
-			case T_COMMENT:
 			case T_DOC_COMMENT:
 				if ($touchedNonIndentableComment) {
 					$touchedNonIndentableComment = false;
@@ -53,6 +52,13 @@ final class LeftAlignComment extends FormatterPass {
 					}, $lines);
 					$this->appendCode(implode($this->newLine, $lines));
 					break;
+				}
+				$this->appendCode($text);
+				break;
+
+			case T_COMMENT:
+				if ($touchedNonIndentableComment) {
+					$touchedNonIndentableComment = false;
 				}
 				$this->appendCode($text);
 				break;
